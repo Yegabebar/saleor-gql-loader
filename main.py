269e5a5b43b4a57dd35e61be389f89eba01951b1
Yegabebar <m.barbegayet@gmail.com>
2021-06-08 15:39:09 +0200

    added basic checks for the csv file passed in input

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from saleor_gql_loader import ETLDataLoader, utils, ETLDataRequester
 import sys
 
 
+# TODO: Move the authentication query in the package utils
 def get_auth_query():
     return """fragment
                 User
@@ -37,37 +38,39 @@ def get_auth_query():
                 }"""
 
 
-def import_mode(etl_data_loader):
+def import_mode(etl_data_loader, product_csv):
     # create a default warehouse
     # warehouse_id = etl_data_loader.create_warehouse()
     # create a default shipping zone associated
     """shipping_zone_id = etl_data_loader.create_shipping_zone(addWarehouses=["79891a8c-7e28-48fd-8472-28edb13467a2"])
     print(shipping_zone_id)"""
 
-    products = [
-        {
-            "name": "tea n",
-            "description": "description for tea a",
-            "category": "green tea",
-            "price": 5.5,
-            "strength": "medium"
-        },
-        {
-            "name": "tea o",
-            "description": "description for tea b",
-            "category": "black tea",
-            "price": 10.5,
-            "strength": "strong"
-        },
-        {
-            "name": "tea p",
-            "description": "description for tea c",
-            "category": "green tea",
-            "price": 9.5,
-            "strength": "light"
-        }
-    ]
+    # products = [
+    #     {
+    #         "name": "tea n",
+    #         "description": "description for tea a",
+    #         "category": "green tea",
+    #         "price": 5.5,
+    #         "strength": "medium"
+    #     },
+    #     {
+    #         "name": "tea o",
+    #         "description": "description for tea b",
+    #         "category": "black tea",
+    #         "price": 10.5,
+    #         "strength": "strong"
+    #     },
+    #     {
+    #         "name": "tea p",
+    #         "description": "description for tea c",
+    #         "category": "green tea",
+    #         "price": 9.5,
+    #         "strength": "light"
+    #     }
+    # ]
 
+    # TODO: Use our csv file to create new products
+    products = {}
 
     # add basic sku to products
     for i, product in enumerate(products):
@@ -136,14 +139,44 @@ def request_mode(etl_data_requester):
 
 
 if __name__ == '__main__':
+    # Variables initialization
+    import_mode_activated = False
+    index_for_csv_arg = 0
+    path_to_csv = ""
+    csv_file = ""
+
+    # Check if the script will be run in import mode or not
+    for index, arg in enumerate(sys.argv):
+        if arg == "-i":
+            # if an argument -i is found, get the next argument (which should be a csv path)
+            try:
+                path_to_csv = sys.argv[index + 1]
+                index_for_csv_arg = index + 1
+                import_mode_activated = True
+            except Exception as e:
+                print("Please provide a path to a csv file for -i: ", e)
+
     # authentication is managed here
     response = utils.graphql_request(get_auth_query())
     auth_token = response['data']['tokenCreate']['token']
 
-    if "-i" in sys.argv:
-        edl = ETLDataLoader(auth_token)
-        print("Script executed in import mode")
-        import_mode(edl)
-    else:
+    if import_mode_activated:
+        if ".csv" in sys.argv[index_for_csv_arg]:
+            try:
+                csv_file = open(path_to_csv)
+                if len(csv_file.read()) != 0:
+                    edl = ETLDataLoader(auth_token)
+                    print("Script executed in import mode")
+                    import_mode(edl, path_to_csv)
+                else:
+                    print("The provided CSV file is empty")
+                csv_file.close()
+            except Exception as e:
+                print("The file could not be opened: ", e)
+        else:
+            print("Please provide a valid path to a CSV file")
+
+    if not import_mode:
+        # if no import argument found, the script will be executed in request mode
         edr = ETLDataRequester(auth_token)
         request_mode(edr)
